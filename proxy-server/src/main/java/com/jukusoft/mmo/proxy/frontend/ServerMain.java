0564a928458ad358206b272d8064120669788a18
juku <kuenzel.justin@t-online.de>
2018-11-07 23:18:41 +0100

    changed main class, added support to set config values with parameters.

--- proxy-server/src/main/java/com/jukusoft/mmo/proxy/frontend/ServerMain.java
+++ proxy-server/src/main/java/com/jukusoft/mmo/proxy/frontend/ServerMain.java
@@ -71,18 +71,20 @@ public class ServerMain {
         //overrides config with params
         for (String param : args) {
             if (param.startsWith("-Config:")) {
-                param = param.substring(7);
+                param = param.substring(8);
                 String[] array = param.split("=");
 
                 if (array.length < 2) {
                     throw new IllegalArgumentException("invalide parameter, -Config parameters requires a '=' to set config value.");
                 }
 
-                String[] array1 = array[0].split(".");
+                String[] array1 = array[0].split("\\.");
 
                 if (array1.length < 2) {
-                    throw new IllegalArgumentException("invalide parameter, -Config parameters requires a '.' in option key to use section.");
+                    throw new IllegalArgumentException("invalide parameter, -Config parameters requires a '.' in option key to use section (current key: '" + array[0] + "').");
                 }
+
+                Log.d(CONFIG_TAG, "set value '" + array[0] + "' = '" + array[1] + "' manually.");
 
                 //set config value
                 Config.set(array1[0], array1[1], array[1]);
