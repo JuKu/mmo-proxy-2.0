e8d2998e1aff66b76f668f61ba1e9631a2fbef2c
juku <kuenzel.justin@t-online.de>
2018-11-11 17:56:25 +0100

    changed database schema.

--- proxy-server/src/main/resources/db/migration/V0001__create_schema.sql
+++ proxy-server/src/main/resources/db/migration/V0001__create_schema.sql
@@ -30,3 +30,27 @@ ALTER TABLE `${prefix}proxy_users`
 --
 ALTER TABLE `${prefix}proxy_users`
 MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT;
+
+-- characters of player
+CREATE TABLE IF NOT EXISTS `${prefix}proxy_characters` (
+  `cid` int(10) NOT NULL,
+  `userID` int(10) NOT NULL DEFAULT '-1',
+  `name` varchar(255) NOT NULL,
+  `data` text NOT NULL,
+  `current_regionID` int(10) NOT NULL DEFAULT '1',
+  `instanceID` int(10) NOT NULL DEFAULT '1',
+  `shardID` int(10) NOT NULL DEFAULT '1',
+  `activated` int(10) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes für die Tabelle `${prefix}characters`
+--
+ALTER TABLE `${prefix}proxy_characters`
+ ADD PRIMARY KEY (`cid`), ADD KEY `userID` (`userID`), ADD KEY `current_regionID` (`current_regionID`);
+
+--
+-- AUTO_INCREMENT für Tabelle `${prefix}characters`
+--
+ALTER TABLE `${prefix}proxy_characters`
+MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT;
