2bed5cb43b23e5a6a17e46e5f762ac21ec12df0e
juku <kuenzel.justin@t-online.de>
2018-11-11 01:58:27 +0100

    added some junit tests for class MySQLConfig.

---
+++ proxy-server/src/test/java/com/jukusoft/mmo/proxy/frontend/database/MySQLConfigTest.java
@@ -0,0 +1,54 @@
+package com.jukusoft.mmo.proxy.frontend.database;
+
+import com.jukusoft.mmo.engine.shared.config.Config;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class MySQLConfigTest {
+
+    @Test
+    public void testContructor () {
+        new MySQLConfig();
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void testLoadWithoutConfig () {
+        MySQLConfig mySQLConfig = new MySQLConfig();
+        mySQLConfig.load();
+    }
+
+    @Test
+    public void testLoad () throws IOException {
+        Config.load(new File("../config/mysql.example.cfg"), false);
+
+        MySQLConfig mySQLConfig = new MySQLConfig();
+        mySQLConfig.load();
+
+        assertEquals("localhost", mySQLConfig.getHost());
+        assertEquals(3306, mySQLConfig.getPort());
+        assertEquals("db1", mySQLConfig.getDatabase());
+        assertEquals("root", mySQLConfig.getUser());
+        assertEquals("testpass", mySQLConfig.getPassword());
+        assertEquals("mmo_", mySQLConfig.getPrefix());
+
+        assertEquals(30, mySQLConfig.getMaxPoolSize());
+        assertEquals(true, mySQLConfig.getJDBCUrl().startsWith("jdbc:mysql://"));
+        assertEquals(250, mySQLConfig.getPrepStmtCacheSize());
+        assertEquals(2048, mySQLConfig.getPrepStmtCacheSqlLimit());
+    }
+
+    @Test
+    public void testGetAndSetPrefix () {
+        MySQLConfig mySQLConfig = new MySQLConfig();
+
+        assertEquals("", mySQLConfig.getPrefix());
+
+        mySQLConfig.setPrefix("mmo_");
+        assertEquals("mmo_", mySQLConfig.getPrefix());
+    }
+
+}
