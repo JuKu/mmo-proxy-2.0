ed38fe22bd9e0e5cae0a330fdd21ffc1a743db82
juku <kuenzel.justin@t-online.de>
2018-11-12 16:18:41 +0100

    fixed some problems in mysql queries.

--- proxy-server/src/main/java/com/jukusoft/mmo/proxy/frontend/login/CharacterService.java
+++ proxy-server/src/main/java/com/jukusoft/mmo/proxy/frontend/login/CharacterService.java
@@ -16,15 +16,15 @@ import java.util.regex.Pattern;
 
 public class CharacterService implements ICharacterService {
 
-    protected static final String SELECT_MY_CHARACTERS = "SELECT * FROM `{prefix}characters` WHERE `userID` = ?; ";
-    protected static final String SELECT_CHARACTER_NAMES = "SELECT * FROM `{prefix}characters` WHERE `name` = ?; ";
-    protected static final String INSERT_CHARACTER = "INSERT INTO `{prefix}characters` (" +
+    protected static final String SELECT_MY_CHARACTERS = "SELECT * FROM `{prefix}proxy_characters` WHERE `userID` = ?; ";
+    protected static final String SELECT_CHARACTER_NAMES = "SELECT * FROM `{prefix}proxy_characters` WHERE `name` = ?; ";
+    protected static final String INSERT_CHARACTER = "INSERT INTO `{prefix}proxy_characters` (" +
             "   `cid`, `name`, `type`, `userID`, `data`, `current_regionID`, `instanceID`, `first_game`, `pos_x`, `pos_y`, `pos_z`, `auto_join`, `visible`, `activated`" +
             ") VALUES (" +
             "   NULL, ?, 'PLAYER', ?, ?, ?, ?, '1', '-1', '-1', '-1', '0', '1', '1'" +
             "); ";
-    protected static final String CHECK_CID_BELONGS_TO_USER = "SELECT * FROM `{prefix}characters` WHERE `userID` = ? AND `cid` = ?; ";
-    protected static final String SELECT_CURRENT_REGION = "SELECT * FROM `{prefix}characters` LEFT JOIN `{prefix}regions` ON `{prefix}characters`.`current_regionID` = `{prefix}regions`.`regionID` WHERE `cid` = ?; ";
+    protected static final String CHECK_CID_BELONGS_TO_USER = "SELECT * FROM `{prefix}proxy_characters` WHERE `userID` = ? AND `cid` = ?; ";
+    protected static final String SELECT_CURRENT_REGION = "SELECT * FROM `{prefix}proxy_characters` LEFT JOIN `{prefix}regions` ON `{prefix}characters`.`current_regionID` = `{prefix}regions`.`regionID` WHERE `cid` = ?; ";
 
     public static final String LOG_TAG = "CharacterService";
 
