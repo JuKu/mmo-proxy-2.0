0e6ba39aaeb5974471c926295fbf55c4dd71190e
juku <kuenzel.justin@t-online.de>
2018-11-21 20:24:14 +0100

    added some junit tests for class RegionMeta.

--- proxy-server/src/test/java/com/jukusoft/mmo/proxy/frontend/database/RegionMetaTest.java
+++ proxy-server/src/test/java/com/jukusoft/mmo/proxy/frontend/database/RegionMetaTest.java
@@ -8,6 +8,11 @@ public class RegionMetaTest {
 
     @Test
     public void testConstructor () {
+        new RegionMeta(10, 20);
+    }
+
+    @Test
+    public void testGetter () {
         RegionMeta region = new RegionMeta(10, 20);
         assertEquals(10, region.getRegionID());
         assertEquals(20, region.getInstanceID());
